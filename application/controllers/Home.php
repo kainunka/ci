982c09cea1f483beec266941a1606c216f35a951
kainunka <joker.200mb@gmail.com>
2016-11-26 14:22:53 +0700

    Add htaccess

---
+++ application/controllers/Home.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: lab
+ * Date: 26/11/2559
+ * Time: 13:22
+ */
+class Home extends CI_Controller
+{
+    public function index() {
+       // echo "Hello World!";
+        $this->load->view('index');
+    }
+
+
+
+}
