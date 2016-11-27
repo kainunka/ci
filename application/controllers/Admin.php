6d51b106e9f5594fd44a57e5a1f3a9087edcb9d8
kainunka <joker.200mb@gmail.com>
2016-11-27 12:07:09 +0700

    sb admin2

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -3,7 +3,10 @@
 class Admin extends CI_Controller
 {
     public function index(){
+        $this->load->view('admin/layout/header_view');
+        $this->load->view('admin/layout/sidebar_view');
         $this->load->view('admin/admin_view');
+        $this->load->view('admin/layout/footer_view');
     }
 
 }
